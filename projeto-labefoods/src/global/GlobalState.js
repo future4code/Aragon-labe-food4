d77339510d5fed71420cb4939da0c5c4b613f811
Pamela Lorenz <pamelamaiara1998@gmail.com>
2022-06-14 15:47:21 -0300

    Parte 1 do dia 2

--- projeto-labefoods/src/global/GlobalState.js
+++ projeto-labefoods/src/global/GlobalState.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useState } from "react";
 import { BASE_URL } from "../constants/urls";
+import { goToAdressPage } from "../routes/cordinator";
 import { convertCPF } from "../utils/convertCPF";
-import GlobalStateContext from "./GlobalStateContext";
+import { GlobalStateContext } from "./GlobalStateContext";
 
 function GlobalState(props) {
+
   const [login, setLogin] = useState({ email: "", password: "" });
 
   const [signUp, setSignUp] = useState({
@@ -14,14 +16,29 @@ function GlobalState(props) {
     password: "",
   });
 
+  const [checker, setChecker] = useState({ email: "", password: "" })
+
+  const [address, setAddress] = useState({
+    "street": "",
+    "number": "",
+    "neighbourhood": "",
+    "city": "",
+    "state": "",
+    "complement": ""
+  })
+
   const states = {
     login: login,
     signUp: signUp,
+    checker: checker,
+    address: address
   };
 
   const setters = {
     setLogin: setLogin,
     setSignUp: setSignUp,
+    setChecker: setChecker,
+    setAddress: setAddress
   };
 
   const postLogin = () => {
@@ -29,7 +46,7 @@ function GlobalState(props) {
       .post(`${BASE_URL}/login`, login)
       .then((res) => {
         console.log(res.data);
-        localStorage.setItem("token", res.data);
+        localStorage.setItem("token", res.data.token);
       })
       .catch((err) => {
         alert("Usuário ou senha inválidos");
@@ -41,8 +58,8 @@ function GlobalState(props) {
     axios
       .post(`${BASE_URL}/signup`, signUp)
       .then((res) => {
-        console.log(res.data)
-        localStorage.setItem("token", res.data)
+        alert("Cadastro realizado com sucesso!")
+        localStorage.setItem("token", res.data.token)
       })
       .catch((err) => {
         alert("Dados inválidos");
@@ -50,14 +67,33 @@ function GlobalState(props) {
       });
   };
 
+  const token = localStorage.getItem("token")
+
+  const addAddress = () => {
+    axios.put(`${BASE_URL}/address`, address, {
+      headers: {
+        auth: token
+      }
+    })
+      .then((res) => {
+        localStorage.setItem("newToken", res.data.token)
+      })
+      .catch((err) => {
+        console.log(err.response)
+      })
+  }
+
   const posts = {
     postLogin: postLogin,
     postSignUp: postSignUp,
   };
+  const puts = {
+    addAddress: addAddress
+  }
 
   //   const getters = {}
 
-  const context = { states, setters, posts };
+  const context = { states, setters, posts, puts };
 
   return (
     <GlobalStateContext.Provider value={context}>
